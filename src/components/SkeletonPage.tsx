1985d4ce55756d526b17aaef5c1d5e6e9e1cf392
lws <3329995814@qq.com>
2026-09-05 20:45:06 +0800

    chore: remove unused skeleton component

--- src/components/SkeletonPage.tsx
+++
@@ -1,3 +0,0 @@
-export function SkeletonPage() {
-  return <main className="layout skeleton-page"><section><div className="skeleton-line wide" /><div className="skeleton-line title-line" /><div className="skeleton-line" /><div className="card skeleton-card"><div className="skeleton-avatar" /><div className="skeleton-line wide" /><div className="skeleton-line" /><div className="skeleton-line short" /></div></section></main>;
-}
